0969502ab8818281b3c4bb0c9b09a1d1e1e2776a
Vitor Hugo <vv001226@gmail.com>
2024-10-08 21:07:20 -0300

    MongoDB

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         await mongoose.connect(
-            `${process.env.MONGODB}`
+            'mongodb+srv://vv001226:i3gfxhB33!@users.ho0a8.mongodb.net/?retryWrites=true&w=majority&appName=Locadoras'
           );
 
     } catch (error) {
